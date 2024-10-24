637f6161fecc94b23e5a6c2f956d2df3d4bbf4b2
pruthviraj5555 <pruthvirajpatil991@.com>
2024-10-24 13:12:53 +0530

    sub1

--- p/sub1.java
+++ p/sub1.java
@@ -3,7 +3,7 @@ package p;
  public class sub1  {
     public static void main ( String[] args){
         int a=10;
-        int b=20;
+        int b=30;
         int c= a-b;
         System.out.println(c);
 
