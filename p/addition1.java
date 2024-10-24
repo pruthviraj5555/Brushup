6fdfd03ede09d3b1592599368855700f8b4157c6
pruthviraj5555 <pruthvirajpatil991@.com>
2024-10-24 11:20:27 +0530

    addition1

---
+++ p/addition1.java
@@ -0,0 +1,12 @@
+package p;
+
+public class addition1 {
+    public static void main ( String[] args){
+        int a=10;
+        int b=20;
+        int c= a+b;
+        System.out.println(c);
+
+
+}
+}
